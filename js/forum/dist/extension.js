671317d563818c839c0d376a4044e7c879004046
7h3ev1l <7h3ev1l@gmail.com>
2016-05-27 15:47:44 +0300

    Fork hard :D

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -19,35 +19,10 @@ System.register('Davis/CustomFooter/main', ['flarum/app', 'flarum/extend', 'flar
                     var _this = this;
 
                     this.isopen = false;
-                    $('body').append('<div id="customFooter" style="position: fixed; bottom: 0; width: 100%;"><div style="position: relative;left: 93%;"><i id="footershow" style="color:#7088a9;cursor:pointer;font-size: 1.5em;" class="fa fa-info-circle"></i><i id="footerhide" style="color:#7088a9;font-size: 1.5em;cursor:pointer;display:none;" class="fa fa-caret-down"></i></div><div class="footercontent" style="display: none;height:' + app.forum.attribute("davis-customfooter.heightofheader") + 'px;">' + app.forum.attribute('davis-customfooter.customtext') + '</div></div>');
+                    $('body').append('<div id="customFooter" style="bottom: 0; width: 100%;"><div class="footercontent" style="">' + app.forum.attribute('davis-customfooter.customtext') + '</div></div>');
                     $('head').append('<style>' + app.forum.attribute("davis-customfooter.cssofheader") + '</style>');
                     $('head').append('<script>' + app.forum.attribute("davis-customfooter.jsofheader") + '</script>');
-                    $('#footershow').click(function () {
-                        $('.footercontent').slideDown('fast', function () {
-                            $('#footershow').slideUp('fast', function () {
-                                $('#footerhide').slideDown('fast');
-                            });
-                        });
-                        _this.isopen = true;
-                    });
-                    $('#footerhide').click(function () {
-                        $('.footercontent').slideUp('fast', function () {
-                            $('#footerhide').slideUp('fast', function () {
-                                $('#footershow').slideDown('fast');
-                            });
-                        });
-                        _this.isopen = false;
-                    });
-                    $('#app').click(function () {
-                        if (_this.isopen) {
-                            $('.footercontent').slideUp('fast', function () {
-                                $('#footerhide').slideUp('fast', function () {
-                                    $('#footershow').slideDown('fast');
-                                });
-                            });
-                        }
-                    });
-                });
+				});
             });
         }
     };
